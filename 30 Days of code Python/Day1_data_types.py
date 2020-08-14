16a01f18d48afee3cf394ac0e446f130dc71c497
Mangeshi <thisismangeshi@gmail.com>
2020-08-14 19:31:28 +0530

    Adds Basic Data types

---
+++ 30 Days of code Python/Day1_data_types.py
@@ -0,0 +1,30 @@
+'''
+Task:
+Complete the code in the editor below. The variables , , and  are already declared and initialized for you. You must:
+Declare  variables: one of type int, one of type double, and one of type String.
+Read  lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your  variables.
+Use the  operator to perform the following operations:
+Print the sum of  plus your int variable on a new line.
+Print the sum of  plus your double variable to a scale of one decimal place on a new line.
+Concatenate  with the string you read as input and print the result on a new line.
+
+Input Format:
+The first line contains an integer that you must sum with .
+The second line contains a double that you must sum with .
+The third line contains a string that you must concatenate with .
+
+Output Format:
+Print the sum of both integers on the first line, the sum of both doubles (scaled to  decimal place) on the second line, and then the two concatenated strings on the third line.
+'''
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+integer = int(input())
+double = float(input())
+string = str(input())
+
+print(i+integer)
+print(d+double)
+print(s + string)
