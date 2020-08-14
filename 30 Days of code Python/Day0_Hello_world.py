6bbfda2042a8040f9235943ef8848f59cfff324d
Mangeshi <thisismangeshi@gmail.com>
2020-08-14 19:27:32 +0530

    Adds Hello World

---
+++ 30 Days of code Python/Day0_Hello_world.py
@@ -0,0 +1,14 @@
+'''
+Task:
+To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line.You've got this!
+
+Input Format:
+A single line of text denoting input string(the variable whose contents must be printed).
+
+Output Format:
+Print Hello, World. on the first line, and the contents of input string on the second line.
+'''
+
+input_string = input()
+print('Hello, World.')
+print(input_string)
